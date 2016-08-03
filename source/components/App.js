6c16bd37b9ab6137bfb38a56135e58e62fb9c1a1
smashingpat <patt.gerr@gmail.com>
2016-08-03 11:38:33 +0200

    cleanup files

--- source/components/App.js
+++ source/components/App.js
@@ -10,19 +10,7 @@ import FilterForm from './FilterForm'
 
 const App = React.createClass({
     componentDidMount() {
-        API.setApiKey(this.props.api)
         this.setApiKey('068C2B8B-9929-9842-9907-88C3FAD88A77088C3179-1451-4D22-AD8B-F80CD4E44072')
-    },
-    changeFilter(filter) {
-        Store.dispatch(Action.changeFilter(filter))
-    },
-    filterItems(items, filter) {
-        if (!filter) return items;
-        return items.filter(item => new RegExp(filter, 'gi').test(item.name))
-    },
-    getItem(items, id) {
-        let result = items.filter(item => item.id === id)[0];
-        return result
     },
     setApiKey(key) {
         Store.dispatch(Action.setApi(key))
